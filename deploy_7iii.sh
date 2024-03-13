ccfdae7f78e7e614766af7a958a8f9cf78198346
nilsmango <simonmilagrolang@icloud.com>
2024-03-13 11:52:11 +0100

    Added deploy script with separate config wich is ignored by git.

---
+++ deploy_7iii.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# sensitive info saved in the config
+source deploy_config.sh
+
+# FTP Deployment using lftp
+lftp -c "open -u $USER,$PASSWORD $HOST; mirror -R --delete --verbose $LOCAL_PATH $REMOTE_PATH"
