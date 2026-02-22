691b6c41f33633c9688a7d7f1f5558b1388e16b0
nilsmango <simonmilagrolang@icloud.com>
2026-02-22 11:12:22 +0100

    changed the deploy to new server

--- deploy_7iii.sh
+++ deploy_7iii.sh
@@ -2,10 +2,6 @@
 # sensitive info saved in the config
 source deploy_config.sh
 
-# FTP Deployment using lftp
-lftp -c "
-set ssl:verify-certificate no
-open -u $USER,$PASSWORD $HOST
-mirror -R --delete --verbose --exclude 'foildata' $LOCAL_PATH $REMOTE_PATH
-"
+# SSH Deployment using rsync
+rsync -avz --delete --exclude 'foildata' -e "ssh -i $SSH_KEY" $LOCAL_PATH $USER@$HOST:$REMOTE_PATH
 
