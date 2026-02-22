7a0539189ae5eb4ef9eb82d5852ccd75e001aea6
nilsmango <simonmilagrolang@icloud.com>
2026-02-22 16:42:31 +0100

    update to deploy

--- deploy_7iii.sh
+++ deploy_7iii.sh
@@ -3,5 +3,5 @@
 source deploy_config.sh
 
 # SSH Deployment using rsync
-rsync -avz --delete --exclude 'foildata' -e "ssh -i $SSH_KEY" $LOCAL_PATH $USER@$HOST:$REMOTE_PATH
+rsync -rlvz --delete --exclude 'foildata' --no-perms --omit-dir-times -e "ssh -i $SSH_KEY" $LOCAL_PATH $USER@$HOST:$REMOTE_PATH
 
